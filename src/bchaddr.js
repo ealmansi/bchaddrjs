dfefaab415d182c751ece3b988ed9936f094ac2b
Emilio Almansi <ealmansi@gmail.com>
2018-11-28 17:08:32 +0100

    Fix prefix: regtest to bchreg.

--- src/bchaddr.js
+++ src/bchaddr.js
@@ -245,7 +245,7 @@ function decodeCashAddress (address) {
     } catch (error) {
     }
   } else {
-    var prefixes = ['bitcoincash', 'bchtest', 'regtest']
+    var prefixes = ['bitcoincash', 'bchtest', 'bchreg']
     for (var i = 0; i < prefixes.length; ++i) {
       try {
         var prefix = prefixes[i]
@@ -278,7 +278,7 @@ function decodeCashAddressWithPrefix (address) {
           type: type
         }
       case 'bchtest':
-      case 'regtest':
+      case 'bchreg':
         return {
           hash: hash,
           format: Format.Cashaddr,
